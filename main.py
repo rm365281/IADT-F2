8362c6f4e49fd43234b610457f5b7527fba2fa9c
Guilherme Groff <guilherme.pgb@hotmail.com>
2025-09-01 10:41:03 +0000

    add restriction ideas

--- main.py
+++ main.py
@@ -1,16 +1,41 @@
 # Restrições
 #
-# 1 - Prioridade diferente para entregas (medicamentos criticos vs entrega regulares)
+# 1 - Prioridade diferente para entregas (medicamentos criticos vs entrega regulares) | 1 ou 0
 # 2 - Capacidade do veiculo
 # 3 - Autonomia (distancia máxima percorrida)
 # 4 - Multiplos veiculos
-# Etc...
+
+# Restrições extras (opcionais)
+# 13 - Carga horaria do motorista (ex: limites de horas de condução)
+# 8 - Restrições de rota (ex: evitar certas ruas, zonas de baixa emissão)
+# 9 - Condições de trânsito em tempo real (MUITO COMPLEXO)
+# 5 - Organizar as caixas dentro do veiculo de forma a aproveitar o espaço (MUITO COMPLEXO)
+# 6 - Separação dos tipos de carga (refrigerados, inflamaveis, etc)
+# 7 - Janelas de tempo para entrega (ex: entregar entre 14:00 e 16:00)
+# 10 - Custos operacionais (ex: minimizar custos de combustível, pedágios)
+# 11 - Restrições legais (ex: limites de peso, regulamentações de transporte)
+# 12 - Preferências do cliente (ex: horários preferenciais de entrega)
+# 14 - Pontos de combustivel (ex: incluir paradas para reabastecimento)
+
+# Ideias
+# Visualização do mapa com rotas reais
+# Diferentes tipos de veiculos (motos, carros, caminhões)
+# Diferentes capacidades e custos dos veiculos
+# Usar cidadades reais
+# Analisar o melhor veiculo para entregar a carga (ex: motos para entregas pequenas e rápidas, caminhões para cargas maiores)
+# Centro de distribuição, a carga vai até o centro e depois é distribuida pelos veiculos menores
+# Informar o tempo estimado de entrega
+# Usuário parametrizar o problema (número de veiculos, capacidade, etc)
+# Interface web
+# Separar o código em frontend e backend
 
 import itertools
 from customer import Customer
 from genetic_algorithm import sort_population, default_problems
 from vrp import VRP
 
+STOP_GENERATION = False
+NUMBER_VEHICLES = 2
 POPULATION_SIZE = 100
 MUTATION_PROBABILITY = 0.5
 
@@ -24,9 +49,10 @@ best_solutions = []
 
 vrp = VRP(customers=customers)
 
-population = vrp.generate_initial_population(POPULATION_SIZE, 2)
+population = vrp.generate_initial_population(POPULATION_SIZE, NUMBER_VEHICLES)
 
-while True:
+generate = True
+while generate:
     generation = next(generation_counter)
 
     population_fitness = [vrp.fitness(individual) for individual in population]
@@ -35,9 +61,6 @@ while True:
 
     best_fitness = vrp.fitness(population[0])
     best_solution = population[0]
-
-    best_fitness_values.append(best_fitness)
-    best_solutions.append(best_solution)
 
     print(f"Generation {generation}: Best fitness = {round(best_fitness, 2)} | Solution: {best_solution}")
 
@@ -49,3 +72,5 @@ while True:
         new_population.append(child)
 
     population = new_population
+
+    generate = not STOP_GENERATION
