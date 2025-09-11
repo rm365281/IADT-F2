579e588d99b4cab2ade700b3b6ce69bf501d5976
Guilherme Groff <guilherme.pgb@hotmail.com>
2025-09-11 08:26:55 -0300

    Implement parents selection with tournament

---
+++ vrp/selection.py
@@ -0,0 +1,38 @@
+import random
+
+from solution import Solution
+
+
+def parents_selection(population: list[Solution]) -> tuple[Solution, Solution]:
+    """
+    Selects two parents from the population using tournament selection.
+    If a population with only two individuals is provided, those two individuals are returned as parents.
+
+    Args:
+        population: List of solutions representing the population.
+
+    Returns:
+        A tuple containing two selected parent solutions.
+    Raises:
+        ValueError: If the population has less than two individuals.
+
+    """
+    if len(population) < 2:
+        raise ValueError("Population must have at least two individuals to select parents.")
+
+    if len(population) == 2:
+        return population[0], population[1]
+
+    tournament_size = 5
+    if len(population) < 5:
+        tournament_size = len(population)
+
+    selected = []
+    for _ in range(2):
+        tournament = random.sample(range(len(population)), tournament_size)
+        winner = max(tournament, key=lambda i: population[i].fitness)
+        selected.append(population[winner])
+        population.remove(population[winner])
+        if len(population) < tournament_size:
+            tournament_size = len(population)
+    return selected[0], selected[1]
